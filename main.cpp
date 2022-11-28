89672edcd8df2b65ce425c7ac9a9f9b372936a54
gabri-rabaglio <115536884+gabri-rabaglio@users.noreply.github.com>
2022-11-28 08:23:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
+int fibonacci(int n) {
+    int a = 0, b = 1, c=0;
+    cout << 1 << endl;
+        for (int i = 2;; i++) {
+          c=a+b;
+          a=b;
+          b=c;
+         if(c>n)break;
+           cout << c << endl;
+        }
+    }
 
-int main() {
-   return 0;
-}
+    int main() {
+        int n = 0;
+        cin >> n;
+            fibonacci(n);
+        return 0;
+    }
